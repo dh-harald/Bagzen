66981c8367637ee66db411dfc2ec73a3c3648f53
Peter Nyilas <dh.harald@gmail.com>
2025-07-08 16:45:47 +0100

    fix: added bag openings on mailbox

--- Events.lua
+++ Events.lua
@@ -23,6 +23,18 @@ function Bagzen:BAG_UPDATE_COOLDOWN()
     Bagzen:ContainerUpdate(BagzenBagFrame, Bagzen.realmname, Bagzen.unitname)
 end
 
+function Bagzen:MAIL_CLOSED()
+    if Bagzen.settings.global.bagframe.close_mail then
+        BagzenBagFrame:Hide()
+    end
+end
+
+function Bagzen:MAIL_SHOW()
+    if Bagzen.settings.global.bagframe.open_mail then
+        BagzenBagFrame:Show()
+    end
+end
+
 function Bagzen:PLAYER_LOGIN()
     Bagzen:ContainerInit(BagzenBagFrame, {0, 1, 2, 3, 4, KEYRING_CONTAINER})
     Bagzen:ContainerUpdate(BagzenBagFrame, Bagzen.realmname, Bagzen.unitname)
@@ -32,5 +44,7 @@ end
 function Bagzen:OnEnable()
     Bagzen:RegisterEvent("BAG_UPDATE")
     Bagzen:RegisterEvent("BAG_UPDATE_COOLDOWN")
+    Bagzen:RegisterEvent("MAIL_CLOSED")
+    Bagzen:RegisterEvent("MAIL_SHOW")
     Bagzen:RegisterEvent("PLAYER_LOGIN")
 end
