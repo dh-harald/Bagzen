4f7dfed6697cd6a842ca39681d581b94e39b1685
Peter Nyilas <dh.harald@gmail.com>
2025-08-23 13:07:31 +0100

    fix: added more tooltip includes, fixed hyperLink on wrath client

--- Tooltip.lua
+++ Tooltip.lua
@@ -9,12 +9,16 @@ end)
 
 local BagzenSetHyperLink = GameTooltip.SetHyperlink
 function GameTooltip.SetHyperlink(self, arg1)
+    Bagzen.Tooltip.itemID = nil
     if arg1 then
         local _, _, linktype = string.find(arg1, "^(.-):(.+)$")
+
+        if string.find(linktype, "^|c%x+|H(.+)") then
+            local _, _, lt = string.find(linktype, "^|c%x+|H(.+)")
+            linktype = lt
+        end
         if linktype == "item" then
             Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(arg1)
-        else
-            Bagzen.Tooltip.itemID = nil
         end
     end
     return BagzenSetHyperLink(self, arg1)
@@ -22,6 +26,7 @@ end
 
 local BagzenSetBagItem = GameTooltip.SetBagItem
 function GameTooltip.SetBagItem(self, container, slot)
+    Bagzen.Tooltip.itemID = nil
     if not container or not slot then
         return BagzenSetBagItem(self, container, slot)
     end
@@ -30,8 +35,6 @@ function GameTooltip.SetBagItem(self, container, slot)
     if itemLink then
         local itemID = Bagzen:LinkToItemID(itemLink)
         Bagzen.Tooltip.itemID = itemID
-    else
-        Bagzen.Tooltip.itemID = nil
     end
 
     return BagzenSetBagItem(self, container, slot)
@@ -39,6 +42,7 @@ end
 
 local BagzenSetInventoryItem = GameTooltip.SetInventoryItem
 function GameTooltip.SetInventoryItem(self, unit, slot)
+    Bagzen.Tooltip.itemID = nil
     if not unit or not slot then
         return BagzenSetInventoryItem(self, unit, slot)
     end
@@ -53,6 +57,7 @@ end
 
 local BagzenSetInboxItem = GameTooltip.SetInboxItem
 function GameTooltip.SetInboxItem(self, index, attachment)
+    Bagzen.Tooltip.itemID = nil
     if not index then
         return BagzenSetInboxItem(self, index, attachment)
     end
@@ -70,6 +75,7 @@ end
 
 local BagzenSetQuestLogItem = GameTooltip.SetQuestLogItem
 function GameTooltip.SetQuestLogItem(self, itemType, index)
+    Bagzen.Tooltip.itemID = nil
     if not itemType or not index then
         return BagzenSetQuestLogItem(self, itemType, index)
     end
@@ -84,6 +90,7 @@ end
 
 local BagzenSetQuestItem = GameTooltip.SetQuestItem
 function GameTooltip.SetQuestItem(self, itemType, index)
+    Bagzen.Tooltip.itemID = nil
     if not itemType or not index then
         return BagzenSetQuestItem(self, itemType, index)
     end
@@ -93,6 +100,104 @@ function GameTooltip.SetQuestItem(self, itemType, index)
         Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
     end
     return BagzenSetQuestItem(self, itemType, index)
+end
+
+local BagzenSetTradeSkillItem = GameTooltip.SetTradeSkillItem
+function GameTooltip.SetTradeSkillItem(self, index, rip)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink
+    if rip ~= nil then
+        itemLink = GetTradeSkillReagentItemLink(index, rip)
+    else
+        itemLink = GetTradeSkillItemLink(index)
+    end
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetTradeSkillItem(self, index, rip)
+end
+
+local BagzenSetAuctionItem = GameTooltip.SetAuctionItem
+function GameTooltip.SetAuctionItem(self, atype, index)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetAuctionItemLink(atype, index)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetAuctionItem(self, atype, index)
+end
+
+local BagzenSetAuctionSellItem = GameTooltip.SetAuctionSellItem
+function GameTooltip.SetAuctionSellItem(self)
+    Bagzen.Tooltip.itemID = nil
+    local itemName = GetAuctionSellItemInfo()
+    if itemName then
+        local itemID = Bagzen:GetItemIDByName(itemName)
+        if itemID then
+            Bagzen.Tooltip.itemID = itemID
+        end
+    end
+    return BagzenSetAuctionSellItem(self)
+end
+
+local BagzenSetLootItem = GameTooltip.SetLootItem
+function GameTooltip.SetLootItem(self, slot)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetLootSlotLink(slot)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetLootItem(self, slot)
+end
+
+local BagzenSetLootRollItem = GameTooltip.SetLootRollItem
+function GameTooltip.SetLootRollItem(self, id)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetLootRollItemLink(id)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetLootRollItem(self, id)
+end
+
+local BagzenSetMerchantItem = GameTooltip.SetMerchantItem
+function GameTooltip.SetMerchantItem(self, index)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetMerchantItemLink(index)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetMerchantItem(self, index)
+end
+
+local BagzenSetBuybackItem = GameTooltip.SetBuybackItem
+function GameTooltip.SetBuybackItem(self, index)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetBuybackItemLink(index)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetBuybackItem(self, index)
+end
+
+local BagzenSetTradePlayerItem = GameTooltip.SetTradePlayerItem
+function GameTooltip.SetTradePlayerItem(self, index)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetTradePlayerItemLink(index)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetTradePlayerItem(self, index)
+end
+
+local BagzenSetTradeTargetItem = GameTooltip.SetTradeTargetItem
+function GameTooltip.SetTradeTargetItem(self, index)
+    Bagzen.Tooltip.itemID = nil
+    local itemLink = GetTradeTargetItemLink(index)
+    if itemLink then
+        Bagzen.Tooltip.itemID = Bagzen:LinkToItemID(itemLink)
+    end
+    return BagzenSetTradeTargetItem(self, index)
 end
 
 function Bagzen:TooltipOnShow(frame)
